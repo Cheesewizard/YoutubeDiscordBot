56246d82f35b2b1fdef81864f44dbd3365845465
Philip <Cheesewizard06@hotmail.com>
2023-04-23 02:43:06 +0100

    Working discord bot that plays youtube music

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.DependencyInjection;
+using DSharpPlus.VoiceNext;
+
+namespace DiscordBot
+{
+    internal class Program
+    {
+        private static Youtube youtube = new Youtube();
+        private static DiscordClient client;
+        private static SlashCommandsExtension slashCommands;
+
+        private static string token => Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting Discord Youtube Bot");
+            await DiscordClient();
+            RegisterSlashCommands();
+
+            // Keep the application running until terminated
+            await Task.Delay(-1);
+        }
+
+        private static async Task DiscordClient()
+        {
+            var config = new DiscordConfiguration
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.All
+            };
+
+            client = new DiscordClient(config);
+
+            // Enable VoiceNext
+            client.UseVoiceNext();
+
+            client.Ready += Client_Ready;
+            client.MessageCreated += Client_MessageCreated;
+
+            await client.ConnectAsync();
+        }
+
+
+        private static async void RegisterSlashCommands()
+        {
+            var slashCommandsConfiguration = new SlashCommandsConfiguration
+            {
+                Services = new ServiceCollection().BuildServiceProvider()
+            };
+
+            slashCommands = client.UseSlashCommands(slashCommandsConfiguration);
+
+            // Register your command modules
+            slashCommands.RegisterCommands<Youtube>();
+
+            // Set up your command handlers
+            slashCommands.SlashCommandExecuted += async (s, e) =>
+            {
+                await e.Context.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+            };
+
+            slashCommands.SlashCommandErrored += async (s, e) =>
+            {
+                await e.Context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder()
+                        .WithContent("An error occurred while executing the command.")
+                        .AsEphemeral(true));
+            };
+        }
+
+        private static async Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            Console.WriteLine("Bot is connected and ready!");
+        }
+
+        private static async Task Client_MessageCreated(DiscordClient sender, MessageCreateEventArgs e)
+        {
+            if (e.Message.Content.StartsWith("philip."))
+            {
+                // Process your text-based commands here if needed
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        private static async Task DiscordClient()
+//        {
+//            var config = new DiscordSocketConfig
+//            {
+//                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+//            };
+
+//            client = new DiscordSocketClient(config);
+
+//            await client.LoginAsync(TokenType.Bot, "MTA5OTA2OTU0MjYxMDc3NjEyNA.GZ4AXO.FFwi0SR-4Qo2qsQYFzcHHl3vYGnVwIwDui1zKo");
+//            await client.StartAsync();
+
+//            client.Ready += Ready;
+//            client.MessageReceived += HandleCommandAsync;
+//        }
+
+//        private static Task Ready()
+//        {
+//            Console.WriteLine("Bot is connected and ready!");
+//            return Task.CompletedTask;
+//        }
+
+//        private static async Task DiscordCommands()
+//        {
+//            var commandsConfig = new CommandServiceConfig { Prefix = "philip." };
+
+//            commands = new CommandService(commandsConfig);
+//            await commands.AddModuleAsync<Youtube>(null);
+//            await commands.AddModuleAsync<MyModule>(null);
+//            commands.CommandExecuted += CommandExecutedAsync;
+//        }
+
+//        private static async Task HandleCommandAsync(SocketMessage messageParam)
+//        {
+//            var message = messageParam as SocketUserMessage;
+//            if (message == null) return;
+
+//            int argPos = 0;
+//            if (!(message.HasStringPrefix("philip.", ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)) || message.Author.IsBot) return;
+
+//            var context = new SocketCommandContext(client, message);
+//            var result = await commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+
+//            if (!result.IsSuccess)
+//            {
+//                Console.WriteLine(result.ErrorReason);
+//            }
+//        }
+
+//        private static async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+//        {
+//            // If the command was successful, we don't need to log anything
+//            if (result.IsSuccess) return;
+
+//            // Otherwise, log the error
+//            await context.Channel.SendMessageAsync($"Error: {result.ErrorReason}");
+//        }
+
+//    }
+//}
---
+++ Youtube.cs
@@ -0,0 +1,175 @@
+﻿using YoutubeExplode;
+using YoutubeExplode.Videos;
+using NAudio.Wave;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.Entities;
+using DSharpPlus;
+using DSharpPlus.VoiceNext;
+using System.Threading;
+
+namespace DiscordBot
+{
+    internal class Youtube : ApplicationCommandModule
+    {
+        private static Queue<Video> songQueue = new();
+        private static YoutubeClient youtube = new();
+        private static CancellationTokenSource tokenSource = new();
+
+        public Youtube()
+        {
+            youtube = new YoutubeClient();
+        }
+
+        [SlashCommand("join", "Join the user's voice channel or the specified channel")]
+        public async Task JoinChannel(InteractionContext context, [Option("channel", "The voice channel to join")] DiscordChannel channel = null)
+        {
+            // Get the audio channel
+            var guildMember = await context.Guild.GetMemberAsync(context.User.Id);
+            channel ??= guildMember?.VoiceState?.Channel;
+
+            if (channel == null)
+            {
+                await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                    .WithContent("You need to be in a voice channel or provide a voice channel as an argument."));
+                return;
+            }
+
+            // Connect to the channel
+            var nextVoice = context.Client.GetVoiceNext();
+            var connection = nextVoice.GetConnection(context.Guild);
+
+            if (connection != null)
+            {
+                connection.Disconnect();
+            }
+
+            connection = await nextVoice.ConnectAsync(channel);
+            await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                .WithContent($"Joined {channel.Name}"));
+        }
+
+        [SlashCommand("queue", "Queue a song using the provided YouTube URL")]
+        public async Task QueueSongAsync(InteractionContext ctx, [Option("url", "The YouTube URL of the song to queue")] string url)
+        {
+            var video = await youtube.Videos.GetAsync(url);
+            if (video != null)
+            {
+                songQueue.Enqueue(video);
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                    .WithContent($"Song queued: {url}"));
+                return;
+            }
+
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                .WithContent($"Song not found: {url}"));
+        }
+
+
+        [SlashCommand("play", "Play the queued songs")]
+        public async Task PlaySongAsync(InteractionContext ctx)
+        {
+            if (songQueue.Count > 0)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+                while (songQueue.Count > 0)
+                {
+                    var video = songQueue.Dequeue();
+                    await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
+                        .WithContent($"Playing: {video.Id}"));
+
+                    tokenSource = new CancellationTokenSource();
+                    await Play(video.Id, tokenSource.Token);
+                }
+
+                await ctx.FollowUpAsync(new DiscordFollowupMessageBuilder()
+                    .WithContent($"No more songs queued"));
+            }
+            else
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                    .WithContent($"No songs in the queue"));
+            }
+        }
+
+
+        [SlashCommand("skip", "Skip the current song")]
+        public async Task SkipSongAsync(InteractionContext ctx)
+        {
+            if (songQueue.Count > 0)
+            {
+                Skip();
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                    .WithContent("Skipping the current song..."));
+            }
+            else
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                    .WithContent("No songs in the queue to skip."));
+            }
+        }
+
+        private async Task Play(string videoId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var video = await youtube.Videos.GetAsync(videoId);
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoId);
+
+                var audioStreamInfo = streamManifest.GetAudioOnlyStreams().OrderByDescending(s => s.Bitrate).FirstOrDefault();
+
+                if (audioStreamInfo != null)
+                {
+                    var stream = await youtube.Videos.Streams.GetAsync(audioStreamInfo);
+                    var tempFilePath = Path.GetTempFileName();
+                    await using (var fileStream = File.OpenWrite(tempFilePath))
+                    {
+                        await stream.CopyToAsync(fileStream);
+                    }
+
+                    using var mediaFoundationReader = new MediaFoundationReader(tempFilePath);
+                    using var pcmStream = WaveFormatConversionStream.CreatePcmStream(mediaFoundationReader);
+                    using var blockAlignedStream = new BlockAlignReductionStream(pcmStream);
+
+                    var player = new WaveOutEvent();
+                    player.Init(blockAlignedStream);
+                    player.Play();
+
+                    player.PlaybackStopped += (sender, args) =>
+                    {
+                        player.Dispose();
+                        blockAlignedStream.Dispose();
+                        File.Delete(tempFilePath);
+                    };
+
+                    while (player.PlaybackState == PlaybackState.Playing)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(1000); // Wait for a second before checking again
+                    }
+                }
+            }
+            catch (OperationCanceledException exception)
+            {
+                Console.WriteLine("Cancelled playing current song");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error playing song, {exception.Message}");
+            }
+        }
+
+        private async void Queue(string videoUrl)
+        {
+            var video = await youtube.Videos.GetAsync(videoUrl);
+            songQueue.Enqueue(video);
+        }
+
+        private void Skip()
+        {
+            tokenSource.Cancel();
+            tokenSource = new CancellationTokenSource();
+        }
+
+    }
+}
